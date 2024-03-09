d9b92ef9408c73ee92149b8f7e1af2c6c715a26d
“sarthaksinha31” <sinha.sarthak31@gmail.com>
2024-03-09 23:36:55 +0530

    updated comments

--- app/controllers/users.py
+++ app/controllers/users.py
@@ -1,3 +1,4 @@
+"""Controller to handle user specific operations"""
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt, current_user
 from src.utils.error_utils import RequestValidationError, UserNotFound
